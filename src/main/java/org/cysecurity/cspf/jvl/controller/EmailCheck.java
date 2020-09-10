29a7ab2ad9bce17ce3691ef01d913b57adb79626
Tyler Agypt <tyler.agypt@checkmarx.com>
2020-09-10 15:40:08 +0000

    Reverting back to poor security practice of SQLi

--- src/main/java/org/cysecurity/cspf/jvl/controller/EmailCheck.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/EmailCheck.java
@@ -43,13 +43,13 @@ public class EmailCheck extends HttpServlet {
                JSONObject json=new JSONObject();
                 if(con!=null && !con.isClosed())
                 {
-                    ResultSet rs=null;
-                    PreparedStatement ps = con.preparedStatement("select * from users where email = ?")
-                    ps.setString(1,email)
-                    ResultSet result = ps.executeQuery();
-                    //Statement stmt = con.createStatement();  
-                    //rs=stmt.executeQuery("select * from users where email ='"+email+"'");
+                    Statement stmt = con.createStatement();  
+                    rs=stmt.executeQuery("select * from users where email ='"+email+"'");
 
+                    //ResultSet rs=null;
+                    //PreparedStatement ps = con.preparedStatement("select * from users where email = ?");
+                    //ps.setString(1,email);
+                    //ResultSet result = ps.executeQuery();
 
                     if (rs.next()) 
                     {  
