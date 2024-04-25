96f8bdeacc61d93bcf45ea7fcdcf4eae72680002
Tyler Agypt <tyler.agypt@checkmarx.com>
2024-04-25 12:31:47 -0400

    Updated version

--- cloudinfra/rmq_2.tf
+++ cloudinfra/rmq_2.tf
@@ -7,7 +7,7 @@ resource "aws_mq_broker" "rmq_2" {
   }
 
   engine_type        = "RabbitMQ"
-  engine_version     = "5.15.9"
+  engine_version     = "5.15.10"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.test.id]
 
